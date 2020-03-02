557f23e74e83741e26b099069d420c95ad81986a
Omario169 <47423327+Omario169@users.noreply.github.com>
2020-03-02 12:37:21 +0000

    store is complete

--- cart_body.php
+++ cart_body.php
@@ -72,7 +72,7 @@
 
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light sticky-top" id="navbar">
-	<a class="navbar-brand home" href="#">Navbar</a>
+	<a class="navbar-brand home" href="#">Hadouken</a>
 	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 	<span class="navbar-toggler-icon"></span>
 	</button>
